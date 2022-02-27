bc3898e1393c3b61a23ce328528f1248cc0b4d95
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-02-27 23:12:08 +0900

    add replication database name setting.

--- mysql/replication/v57/slave/sql/initialize.sql
+++ mysql/replication/v57/slave/sql/initialize.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS local_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS local_db_replication CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON local_db.* TO 'root'@'%';
 
